efd275ab11c3c7e02172e30f9624d1f117e97ebd
HR <hagairossman@gmail.com>
2022-01-10 08:54:35 +0200

    start first example

--- pymsm/model.py
+++ pymsm/model.py
@@ -1,12 +1,26 @@
 import numpy as np
 
 
-class SinglePath():
+class SinglePatient:
+    def __init__(
+        self,
+        covariates: np.ndarray = None,
+        visited_states: np.ndarray = None,
+        time_at_each_state: np.ndarray = None,
+        id: int = None,
+    ) -> None:
+        self.covariates = covariates
+        self.visited_states = visited_states
+        self.time_at_each_state = time_at_each_state
+        self.id = id
+
+
+
+class MSM:
     def __init__(self) -> None:
         pass
 
 
-class MSM():
-    def __init__(self) -> None:
-        pass
+if __name__ == "__main__":
+    pass
 

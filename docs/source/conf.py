0df100c2dd4600af83a16e23d2082a3ec005c5d3
HR <hagairossman@gmail.com>
2022-01-20 21:54:06 +0200

    docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,14 +1,9 @@
 # Configuration file for the Sphinx documentation builder.
 
 import sys
-import os
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath(".."))
-import pymsm
+import pathlib
+import sys
+sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 
 
 # -- Project information

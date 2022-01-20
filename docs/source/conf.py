6a7e931157c810f7ae32f5b2da22a00ad895c944
HR <hagairossman@gmail.com>
2022-01-20 22:21:20 +0200

    x

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,14 +20,12 @@ version = '0.1.0'
 # -- General configuration
 
 extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
+    "sphinx.ext.coverage",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosectionlabel",
     "sphinx.ext.napoleon",
     "nbsphinx",
-
 ]
 
 intersphinx_mapping = {

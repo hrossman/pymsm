315d65fe668176c24b7348d895c76f573884904d
HR <hagairossman@gmail.com>
2022-01-25 10:57:43 +0200

    new dataset lodaers

--- pymsm/utils.py
+++ pymsm/utils.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from typing import List
 from scipy.interpolate import interp1d
+from sklearn.preprocessing import OneHotEncoder
 
 
 def stepfunc(xs: np.ndarray, ys: np.ndarray) -> interp1d:
@@ -11,11 +12,19 @@ def stepfunc(xs: np.ndarray, ys: np.ndarray) -> interp1d:
     return interp1d(xs, ys, kind="previous", fill_value=np.nan, bounds_error=False)
 
 
-def plot_stackplot(times:np.ndarray ,cifs:List, labels:List=None, ax=None):
+def plot_stackplot(times: np.ndarray, cifs: List, labels: List = None, ax=None):
     if ax is None:
-        fig, ax = plt.subplots() 
+        fig, ax = plt.subplots()
     ax.stackplot(times, *cifs, labels=labels)
     ax.legend(loc="upper left")
     ax.set_xlim(times[0], times[-1])
     plt.show()
     return ax
+
+
+def get_categorical_columns(df: pd.DataFrame, cat_cols: List) -> pd.DataFrame:
+    encoder = OneHotEncoder(drop="first", sparse=False)
+    new_df = pd.DataFrame(encoder.fit_transform(df[cat_cols]), dtype=int)
+    new_df.columns = encoder.get_feature_names_out(cat_cols)
+    return new_df
+

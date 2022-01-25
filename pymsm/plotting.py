42160459b74120c6b01f449115a9d7ac6a2782c1
HR <hagairossman@gmail.com>
2022-01-26 01:02:02 +0200

    stackplot ready

--- pymsm/plotting.py
+++ pymsm/plotting.py
@@ -1,6 +1,8 @@
+from tkinter import font
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from pytest import fail
 from pymsm.competing_risks_model import CompetingRisksModel
 from lifelines import AalenJohansenFitter
 from typing import List
@@ -8,46 +10,96 @@ from typing import List
 
 def stackplot(
     data,
+    duration_col: str = None,
+    event_col: str = None,
     order_top: List = None,
     order_bottom: List = None,
     times: np.ndarray = None,
+    labels: List[str] = None,
     ax=None,
 ):
     if times is None:
-        times = np.sort(data["time"].unique())
+        times = np.sort(data[duration_col].unique())
+
+    fontsize=18
 
     if ax is None:
         fig, ax = plt.subplots(1, 1)
         ax.set_ylim(0, 1)
         ax.set_xlim(0, times[-1])
-        ax.set_ylabel("Probability")
-        ax.set_xlabel("t")
+        ax.set_ylabel("Probability", fontsize=fontsize)
+        ax.set_xlabel("t", fontsize=fontsize)
 
-    cumulative_densities = [0]
-    for status in [1, 2]:
-        T = data["time"]
-        E = data["status"]
+    failure_types = np.sort(data[event_col].unique())
+    if labels is None:
+        labels = dict(zip(failure_types, [str(f) for f in failure_types]))
+
+    cumulative_densities = dict(zip(failure_types, ([0] * len(failure_types))))
+    for failure_type in failure_types:
+        T = data[duration_col]
+        E = data[event_col]
         ajf = AalenJohansenFitter()
-        ajf.fit(T, E, event_of_interest=status)
-        cumulative_densities.append(ajf.predict(times, interpolate=True))
+        ajf.fit(T, E, event_of_interest=failure_type)
+        cumulative_densities[failure_type] = ajf.predict(times, interpolate=True)
+
+    
 
     cifs_top = []
     for i, failure_type in enumerate(order_top):
+        color = f"C{failure_type}"
         cif = 1 - cumulative_densities[failure_type].values
-        times = cumulative_densities[failure_type].index
-        if i != 0:
+        if i == 0:
+            ax.fill_between(times, 1, cif, alpha=0.8, color=color)
+            ax.text(
+                x=times[-1] * 1.02,
+                y=(cif[-1] + (1 - cif[-1]) / 2),
+                s=labels[failure_type],
+                fontsize=fontsize,
+                color=color,
+            )
+        else:
             cif = cif - cifs_top[i - 1]
+            ax.fill_between(times, cifs_top[i - 1], cif, alpha=0.8, color=color)
+            ax.text(
+                x=times[-1] * 1.02,
+                y=(cif[-1] + (cifs_top[i - 1][-1] - cif[-1]) / 2),
+                s=labels[failure_type],
+                fontsize=fontsize,
+                color=color,
+            )
         ax.plot(times, cif, color="k")
-        ax.fill_between(times, 1, cif, alpha=0.8)
         cifs_top.append(cif)
 
     cifs_bottom = []
     for i, failure_type in enumerate(order_bottom):
+        color = f"C{failure_type}"
         cif = cumulative_densities[failure_type].values
-        times = cumulative_densities[failure_type].index
-        if i != 0:
+        if i == 0:
+            ax.fill_between(times, 0, cif, alpha=0.8, color=color)
+            ax.text(
+                x=times[-1] * 1.02,
+                y=((cif[-1]) / 2),
+                s=labels[failure_type],
+                fontsize=fontsize,
+                color=color,
+            )
+        else:
             cif = cif + cifs_bottom[i - 1]
+            ax.fill_between(
+                times,
+                cifs_bottom[i - 1],
+                cif,
+                alpha=0.8,
+                label=labels[failure_type],
+                color=color,
+            )
+            ax.text(
+                x=times[-1] * 1.02,
+                y=(cif[-1] + (cifs_bottom[i - 1][-1] - cif[-1]) / 2),
+                s=labels[failure_type],
+                fontsize=fontsize,
+                color=color,
+            )
         ax.plot(times, cif, color="k")
-        ax.fill_between(times, 0, cif, alpha=0.8)
-        cifs_top.append(cif)
+        cifs_bottom.append(cif)
 

3e7e79bfc666429921738c9da56857c7aa571437
ayya-keshet <ayya.keshet@gmail.com>
2022-01-31 14:40:31 +0200

    change package name

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='PyMSM',
-    version='0.0.2',
+    name='pymsm',
+    version='0.0.1',
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     description='Python implementation of a Multi-state competing risk model',

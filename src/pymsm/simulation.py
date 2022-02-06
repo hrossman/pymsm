69ba47e2ec64b2d32a56c15ceb3e620ff0752b3e
HR <hagairossman@gmail.com>
2022-02-06 20:58:29 +0200

    sim complete

--- src/pymsm/simulation.py
+++ src/pymsm/simulation.py
@@ -10,40 +10,33 @@ from pymsm.multi_state_competing_risks_model import (
 
 
 class MultiStateSimulator(MultiStateModel):
+
+    competing_risks_models_list: List[Dict]
+    terminal_states: List[int]
+    update_covariates_fn: Callable[[Series, int, int, float, float], Series]
+    covariate_names: List[str]
+
     def __init__(
         self,
-        multistate_model_dict: Dict = {
-            "terminal_states": [],
-            "update_covariates_fn": default_update_covariates_function,
-            "covariate_names": [],
-            "competing_risks_models_list": [
-                {
-                    "origin_state": None,
-                    "target_states": [],
-                    "model_defs": {"coefs": None, "baseline_hazard": None},
-                }
-            ],
-        },
-        covariate_data: DataFrame = None,
+        competing_risks_models_list,
+        terminal_states,
+        update_covariates_fn=default_update_covariates_function,
+        covariate_names=None,
     ):
 
         # Configure the MSM
         super().__init__(
             dataset=None,
-            terminal_states=multistate_model_dict["terminal_states"],
-            update_covariates_fn=multistate_model_dict["update_covariates_fn"],
-            covariate_names=multistate_model_dict["covariate_names"],
+            terminal_states=terminal_states,
+            update_covariates_fn=update_covariates_fn,
+            covariate_names=covariate_names,
             event_specific_fitter=ManualCoxWrapper,
             competing_risk_data_format=True,
         )
 
         # Configure each competing risks model
-        for competing_risks_model_dict in multistate_model_dict[
-            "competing_risks_models_list"
-        ]:
+        for competing_risks_model_dict in competing_risks_models_list:
             self._configure_competing_risks_model(competing_risks_model_dict)
-
-        self.covariate_data = None
 
     def _configure_competing_risks_model(self, competing_risks_model_dict):
         origin_state = competing_risks_model_dict["origin_state"]
@@ -88,25 +81,55 @@ class MultiStateSimulator(MultiStateModel):
 
 
 def test_on_rossi():
-    # Load roossi dataset
+    # Load rossi dataset
+    from lifelines.datasets import load_rossi
+
+    rossi = load_rossi()
+    from lifelines import CoxPHFitter
+
+    cph = CoxPHFitter()
+    cph.fit(rossi, duration_col="week", event_col="arrest")
+    baseline_hazard = cph.baseline_hazard_["baseline hazard"]
+    coefs = cph.params_
+
     from pymsm.datasets import load_rossi_competing_risk_data
 
     rossi_competing_risk_data, covariate_names = load_rossi_competing_risk_data()
 
     # Define the full model
-    multistate_model_dict = {
-        "terminal_states": [2],
-        "update_covariates_fn": default_update_covariates_function,
-        "covariate_names": ["fin", "age", "race", "wexp", "mar", "paro", "prio"],
-        "competing_risks_models_list": [
-            {
-                "origin_state": 1,
-                "target_states": [2],
-                "model_defs": {"coefs": coefs, "baseline_hazard": baseline_hazard},
-            }
-        ],
-    }
+    competing_risks_models_list = [
+        {
+            "origin_state": 1,
+            "target_states": [2],
+            "model_defs": {"coefs": coefs, "baseline_hazard": baseline_hazard},
+        }
+    ]
+
+    # Configure the simulator
+    mssim = MultiStateSimulator(
+        competing_risks_models_list,
+        terminal_states=[2],
+        update_covariates_fn=default_update_covariates_function,
+        covariate_names=covariate_names,
+    )
+
+    # Run simulation
+    mc_paths = mssim.run_monte_carlo_simulation(
+        sample_covariates=rossi_competing_risk_data.loc[0, covariate_names],
+        origin_state=1,
+        current_time=0,
+        n_random_samples=5,
+        max_transitions=10,
+    )
+    # Print paths
+    for mc_path in mc_paths:
+        states = mc_path.states
+        time_at_each_state = mc_path.time_at_each_state
+        print(states)
+        print(time_at_each_state)
 
 
 if __name__ == "__main__":
     test_on_rossi()
+    print("done")
+

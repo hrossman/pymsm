2aae6fdd032f68c110cce525d729aa7c680c7e75
HR <hagairossman@gmail.com>
2022-02-08 12:41:47 +0200

    setup.py 3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='pymsm',
-    version='0.0.2',
+    version='0.0.3',
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     description='Python implementation of a Multi-state competing risk model',

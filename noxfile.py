a489cdbe44f4384464e8c288673d85e48cca898a
HR <hagairossman@gmail.com>
2022-02-24 23:12:56 +0200

    x

--- noxfile.py
+++ noxfile.py
@@ -1,28 +1,12 @@
 import nox
-import tempfile
-from nox.sessions import Session
 
 locations = "src", "tests", "noxfile.py"
-
-
-# def install_with_constraints(session, *args, **kwargs):
-#     with tempfile.NamedTemporaryFile() as requirements:
-#         session.run(
-#             "poetry",
-#             "export",
-#             "--dev",
-#             "--format=requirements.txt",
-#             f"--output={requirements.name}",
-#             external=True,
-#         )
-#         session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
 @nox.session(python=["3.8"])
 def tests(session):
     session.run("poetry", "install", external=True)
     session.run("pytest")
-    # session.run("pytest", "--cov")
 
 
 @nox.session(python="3.8")
@@ -30,18 +14,3 @@ def black(session):
     args = session.posargs or locations
     session.install("black")
     session.run("black", *args)
-
-
-# @nox.session(python=["3.8"])
-# def lint(session):
-#     args = session.posargs or locations
-#     session.install("flake8")
-#     session.run("flake8", *args)
-
-
-# @nox.session(python="3.8")
-# def coverage(session: Session) -> None:
-#     """Upload coverage data."""
-#     install_with_constraints(session, "coverage[toml]", "codecov")
-#     session.run("coverage", "xml", "--fail-under=0")
-#     session.run("codecov", *session.posargs)

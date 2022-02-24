d01138d1c1828612ba1b8ca517a9101a553385cf
HR <hagairossman@gmail.com>
2022-02-24 11:21:10 +0200

    nox

--- noxfile.py
+++ noxfile.py
@@ -10,11 +10,11 @@ def tests(session):
 locations = "src", "tests", "noxfile.py"
 
 
-@nox.session(python=["3.8"])
-def lint(session):
-    args = session.posargs or locations
-    session.install("flake8")
-    session.run("flake8", *args)
+# @nox.session(python=["3.8"])
+# def lint(session):
+#     args = session.posargs or locations
+#     session.install("flake8")
+#     session.run("flake8", *args)
 
 
 @nox.session(python="3.8")

8e53a77c01c06828282cfa231c150343f929f0c5
HR <hagairossman@gmail.com>
2022-02-24 11:36:36 +0200

    nox

--- noxfile.py
+++ noxfile.py
@@ -1,13 +1,39 @@
 import nox
+from nox.sessions import Session
+
+locations = "src", "tests", "noxfile.py"
+
+
+def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
+    """Install packages constrained by Poetry's lock file.
+    This function is a wrapper for nox.sessions.Session.install. It
+    invokes pip to install packages inside of the session's virtualenv.
+    Additionally, pip is passed a constraints file generated from
+    Poetry's lock file, to ensure that the packages are pinned to the
+    versions specified in poetry.lock. This allows you to manage the
+    packages as Poetry development dependencies.
+    Arguments:
+        session: The Session object.
+        args: Command-line arguments for pip.
+        kwargs: Additional keyword arguments for Session.install.
+    """
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
 @nox.session(python=["3.8"])
 def tests(session):
     session.run("poetry", "install", external=True)
     session.run("pytest", "--cov")
-
-
-locations = "src", "tests", "noxfile.py"
 
 
 # @nox.session(python=["3.8"])
